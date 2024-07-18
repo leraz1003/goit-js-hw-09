9213523e8ed41862745911c9374a515cb4486023
Valeriia Zubrytska <valeriia.zubrytska@gmail.com>
2024-07-18 11:46:56 +0300

    add-task2-task1

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,56 @@
-console.log('Form');
+
+const formData = {
+  email: "",
+  message: ""
+};
+
+const form = document.querySelector('.feedback-form');
+
+const savedData = localStorage.getItem('feedback-form-state');
+
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formData.email = parsedData.email;
+  formData.message = parsedData.message;
+
+  if (form.elements.email.value === '') {
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+}
+
+
+form.addEventListener('input', (evt) => {
+
+  if (evt.target.tagName === 'INPUT' && evt.target.name === 'email') {
+    formData.email = evt.target.value.trim();
+  }
+
+  if (evt.target.tagName === 'TEXTAREA' && evt.target.name === 'message') {
+    formData.message = evt.target.value.trim();
+  }
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+})
+
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  if (formData.email !== '' && formData.message !== '') {
+    console.log(formData);
+
+    form.reset();
+    localStorage.removeItem('feedback-form-state');
+
+    formData.email = '';
+    formData.message = '';
+
+  } else
+    alert('Fill please all fields');
+});
+
+
+
